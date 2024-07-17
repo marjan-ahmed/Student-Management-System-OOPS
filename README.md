0b0819066c30afd689f93100bcd9ddb7c83d28e3
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-07-17 20:31:45 +0500

    using oops: sms/index.ts

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Student-Management-System-OOPS
+# Student Management System
+## Description
+The Student Management System is a command-line application built using TypeScript and the inquirer library for user interaction. The system allows for the management of student information including adding students, enrolling them in courses, viewing balances, paying fees, and displaying student status. The project uses object-oriented programming (OOP) principles to ensure a modular and maintainable codebase.
+
+## Features
+- Add new students 
+- Enroll students in courses
+- View student balances
+- Pay student fees
+- Display student status
+
+## Technologies Used
+- TypeScript
+- Node.js
+- Inquirer.js
+- Chalk
+
+## Installation
+
+### Clone the repository:
+
+```sh
+git clone https://github.com/marjan-ahmed/Student-Management-System-OOPS.git
+cd student-management-system
+
+```
+### Install the dependencies:
+
+```sh
+npm install
+```
+
+# Contributing
+Contributions are welcome! Please create a pull request with your changes.
+
+# License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Result
+![RESULT](result.png)
---
+++ index.ts
@@ -0,0 +1,191 @@
+#! /usr/bin/env
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Student Class
+class Student {
+    static counter = 10000;
+    id: number;
+    name: string;
+    courses: string[];
+    balance: number;
+
+    constructor(name: string) {
+        this.id = Student.counter++;
+        this.name = name;
+        this.courses = [];
+        this.balance = 100;
+    }
+
+    // Enroll students in a course
+    enrollCourse(course: string) {
+        this.courses.push(course);
+    }
+
+    // View student balance
+    viewBalance() {
+        console.log(chalk.blueBright(`\nBalance for ${this.name} is ${this.balance}\n`));
+    }
+
+    // Pay student fees
+    payFees(amount: number) {
+        this.balance -= amount;
+        console.log(chalk.underline.bold(`\n$${amount} Fees paid successfully for ${this.name}`));
+        console.log(chalk.underline(`Remaining Balance: $${this.balance}\n`));
+    }
+
+    // Display student status
+    displayStatus() {
+        console.log(chalk.yellow(`\nStudent ID: ${this.id}`));
+        console.log(chalk.yellow(`Student Name: ${this.name}`));
+        console.log(chalk.yellow(`Student Courses: ${this.courses}`));
+        console.log(chalk.yellow(`Balance: ${this.balance}\n`));
+    }
+}
+
+class StudentManager {
+    students: Student[];
+
+    constructor() {
+        this.students = [];
+    }
+
+    // Add a new student
+    addStudent(name: string) {
+        let student = new Student(name);
+        this.students.push(student);
+        console.log(chalk.green(`\n Student: ${name} added successfully, Student ID: ${student.id}\n`));
+    }
+
+    enrollStudent(studentId: number, course: string) {
+        let student = this.findStudent(studentId);
+        if (student) {
+            student.enrollCourse(course);
+            console.log(chalk.green(`\n ${student.name} enrolled in ${course} successfully!\n`));
+        }
+    }
+
+    // View student balance
+    viewStdBalance(studentId: number) {
+        let student = this.findStudent(studentId);
+        if (student) {
+            student.viewBalance();
+        } else {
+            console.log(chalk.red("\nStudent not found! Please enter a correct student ID.\n"));
+        }
+    }
+
+    // Pay student fees
+    payStudentFees(studentId: number, amount: number) {
+        let student = this.findStudent(studentId);
+        if (student) {
+            student.payFees(amount);
+        } else {
+            console.log(chalk.red("\nStudent not found! Please enter a correct student ID.\n"));
+        }
+    }
+
+    // Show student status
+    showStudentStatus(studentId: number) {
+        let student = this.findStudent(studentId);
+        if (student) {
+            student.displayStatus();
+        }
+    }
+
+    // Find student by student ID
+    findStudent(studentId: number) {
+        return this.students.find(std => std.id === studentId);
+    }
+}
+
+// Main function to run the program
+async function main() {
+    console.log(chalk.magentaBright("\t\nWelcome to Student Management System... By Marjan"));
+    console.log("-".repeat(50));
+    console.log("\n");
+
+    let studentManager = new StudentManager();
+
+    while (true) {
+        let choices = await inquirer.prompt({
+            name: "option",
+            type: "list",
+            message: "Select an option:",
+            choices: [
+                { name: "Add a Student", value: 1 },
+                { name: "Enroll Student", value: 2 },
+                { name: "View Student Balance", value: 3 },
+                { name: "Pay fees", value: 4 },
+                { name: "Show Status", value: 5 },
+                { name: "Exit", value: "exit" }
+            ]
+        });
+
+        switch (choices.option) {
+            case 1:
+                let studentInfo = await inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "Enter student name:"
+                    }
+                ]);
+                studentManager.addStudent(studentInfo.name);
+                break;
+            case 2:
+                let enrollStudent = await inquirer.prompt([
+                    {
+                        name: "student_Id",
+                        type: "input",
+                        message: "Enter student ID:"
+                    },
+                    {
+                        name: "course",
+                        type: "input",
+                        message: "Enter course name:"
+                    }
+                ]);
+                studentManager.enrollStudent(parseInt(enrollStudent.student_Id), enrollStudent.course);
+                break;
+            case 3:
+                let balanceInput = await inquirer.prompt({
+                    name: "student_Id",
+                    type: "input",
+                    message: "Enter student ID: "
+                });
+                studentManager.viewStdBalance(parseInt(balanceInput.student_Id));
+                break;
+            case 4:
+                let feesInput = await inquirer.prompt([
+                    {
+                        name: "student_Id",
+                        type: "input",
+                        message: "Enter student ID: "
+                    },
+                    {
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to pay: "
+                    }
+                ]);
+                studentManager.payStudentFees(parseInt(feesInput.student_Id), feesInput.amount);
+                break;
+            case 5:
+                let statusInput = await inquirer.prompt([
+                    {
+                        name: "student_Id",
+                        type: "input",
+                        message: "Enter student ID: "
+                    }
+                ]);
+                studentManager.showStudentStatus(parseInt(statusInput.student_Id));
+                break;
+            case "exit":
+                console.log("Exiting the program...");
+                process.exit();
+        }
+    }
+}
+
+main();
